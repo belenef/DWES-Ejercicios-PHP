fc45393cfcfea902e7f4dc1d5a9b4333776e2dd9
belenef <beleneezz@gmail.com>
2025-09-16 19:01:46 +0200

    Nuevos archivos y organización (carpetas para cada tema)

---
+++ htdocs/2.1/rrss.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>RRSS</title>
+    </head>
+
+    <body>
+
+        <header>
+            <?php
+
+                require("cabecera.inc.php");
+             ?> 
+        </header>
+
+        
+        <nav>
+           <a href="principal.php">principal.php</a>
+        </nav>
+
+        <section>
+            <p>Sígueme en mis redes sociales</p>
+
+             <a href="http://www.facebook.com">
+                <img src="../2.1/Facebook.jpg" alt="Facebook" width="50" height="50">
+            </a>
+
+            <a href="http://www.x.com">
+                <img src="../2.1/Twitter.jpg" alt="Twitter" width="50" height="50">
+            </a>
+        </section>
+        
+        <footer>
+              <?php
+
+                require(__DIR__ . '/../2.2/footer.inc.php');
+             ?> 
+        </footer>
+        
+    </body>
+</html>
